385898f41192085d6b051598a1214e52b07a5cbd
favasee <156898469+favasee@users.noreply.github.com>
2024-01-20 19:36:55 +0530

    Add files via upload

---
+++ tnx/scripts.js
@@ -0,0 +1,29 @@
+function runAnimations() {
+  setTimeout(() => {
+    document.querySelector("#id-5327").classList.add("animationClass-5327");
+    document.querySelector("#id-5332").classList.add("animationClass-5332");
+    document.querySelector("#id-5335").classList.add("animationClass-5335");
+    document
+      .querySelector(".node-5333-0")
+      .classList.add("animationClass-5333-5333-0");
+    document.querySelector("#id-5333").classList.add("animationClass-5333");
+    document
+      .querySelector(".node-5334-0")
+      .classList.add("animationClass-5334-5334-0");
+    document.querySelector("#id-5334").classList.add("animationClass-5334");
+    document
+      .querySelector(".node-5336-0")
+      .classList.add("animationClass-5336-5336-0");
+    document.querySelector("#id-5336").classList.add("animationClass-5336");
+    document.querySelector("#id-5337").classList.add("animationClass-5337");
+    document.querySelector("#id-5338").classList.add("animationClass-5338");
+    document
+      .querySelector("#id-5338 > div:first-child")
+      .classList.add("bgClass-6834");
+    document.querySelector("#id-6822").classList.add("animationClass-6822");
+    document
+      .querySelector("#id-6822 > div:first-child")
+      .classList.add("bgClass-6835");
+  }, 800);
+}
+runAnimations();
